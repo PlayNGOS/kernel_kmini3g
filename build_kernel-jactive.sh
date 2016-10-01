bb732e85086a9ec71ca96aeab69573a7eb8373bb
gugu0das <gugu0das@gmail.com>
2016-10-01 21:46:47 +0900

    Toolchain : Change Linaro 4.9.4

---
+++ build_kernel-jactive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/arm-cortex-linux-gnueabi-linaro_4.9.4-2015.06/bin/arm-cortex-linux-gnueabi-
+
+mkdir output
+
+make -C $(pwd) O=output msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
