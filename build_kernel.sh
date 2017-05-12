b4c889a337e122f102dcd7eebe1a2e45a523c618
kn06497 <kn06497@hotmail.com>
2017-05-12 10:51:18 -0500

    build: update build script
    Signed-off-by: kn06497 <kn06497@hotmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-
+export CROSS_COMPILE=$(pwd)/armeb-linux-gnueabihf-linaro_5.3.1-2016.05/bin/armeb-linux-gnueabihf-
 mkdir output
 
-make -C $(pwd) O=output msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+export VARIANT_DEFCONFIG=kmini_defconfig
+make -C $(pwd) O=output  kmini_defconfig
 make -j64 -C $(pwd) O=output
-
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
